9d2261709846547f2db0c8bb0cc64fb6168c5092
cezarybednarz <cezary.bednarz@gmail.com>
2020-05-02 20:34:19 +0200

    get request wrzuca id

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import jsonString from "./quizdata.js";
 const quizJson = JSON.parse(jsonString).quiz;
-console.log(Object.keys(quizJson));
-for (var i = 0; i < Object.keys(quizJson).length; i++) {
-    console.log(quizJson[i].id);
-}
 let questionTable = document.getElementById("question-list");
 for (var i = 0; Object.keys(quizJson).length; i++) {
+    var questionId = quizJson[i].id;
     questionTable.insertAdjacentHTML('afterbegin', `<tr>
         <td width="5%"><i class="fa fa-bell-o"></i></td>
-        <td>${quizJson[i].id}</td>
-        <td class="level-right"><a class="button is-small is-primary" href="#">Start</a></td>
+        <td>${questionId}</td>
+        <td class="level-right"><a class="button is-small is-primary" href="quiz.html?id=${questionId}">Start</a></td>
     </tr>`);
 }
-console.log(questionTable);
 //# sourceMappingURL=index.js.map
