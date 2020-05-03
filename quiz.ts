6d3fb9b1658cfda20cfff565fb18f03bee20ae07
cezarybednarz <cezary.bednarz@gmail.com>
2020-05-03 15:51:14 +0200

    dziala wyswietlanie pytania (bez abcd)

--- quiz.ts
+++ quiz.ts
@@ -1,5 +1,7 @@
-import jsonString from "./quizdata.js";
+import jsonString from "./quizdata.js"
+
 const quizJson = JSON.parse(jsonString).quiz;
+
 function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split('&');
@@ -11,33 +13,43 @@ function getQueryVariable(variable) {
     }
     console.log('Query variable %s not found', variable);
 }
-function getQuizIndex(quiz) {
+
+function getQuizIndex(quiz: string): number {
     var index = -1;
-    for (var i = 0; i < Object.keys(quizJson).length; i++) {
-        if (quizJson[i].id === quiz) {
+    for(var i = 0; i < Object.keys(quizJson).length; i++) {
+        if(quizJson[i].id === quiz) {
             index = i;
             break;
         }
     }
     return index;
 }
-function getQuestionById(quiz, questionId) {
+
+function getQuestionById(quiz: string, questionId: number) {
     var index = getQuizIndex(quiz);
     document.getElementById('question').textContent = quizJson[index].get;
     return quizJson[index].questions[questionId];
 }
-function getNumberOfQuestions(quiz) {
+
+function getNumberOfQuestions(quiz : string): number {
     var index = getQuizIndex(quiz);
     return Object.keys(quizJson[index].questions).length;
 }
-function viewQuestion(quiz, questionId) {
+
+function viewQuestion(quiz: string, questionId: number) {
     var percentDone = 100 * questionId / getNumberOfQuestions(quiz);
     document.getElementById("progress-bar").setAttribute("value", String(percentDone));
     document.getElementById('question').textContent = getQuestionById(quizId, currentQuestion).question;
 }
+
 const quizId = getQueryVariable("id");
 let quizName = document.getElementById("quiz-name");
-quizName.insertAdjacentHTML('afterbegin', quizId);
+quizName.insertAdjacentHTML('afterbegin', quizId)
+
+
+
+
 let currentQuestion = 0;
 viewQuestion(quizId, currentQuestion);
-//# sourceMappingURL=quiz.js.map
+
+
