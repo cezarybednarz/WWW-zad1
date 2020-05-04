3bb6a9db3c104e71fdf3e75e75ef96b2d26d51db
cezarybednarz <cezary.bednarz@gmail.com>
2020-05-04 18:27:26 +0200

    ostateczna wersja

--- quizdata.ts
+++ quizdata.ts
@@ -45,19 +45,56 @@ export default `{
             ]
         },
         {
-            "id": "debug",
-            "penalty": "2",
+            "id": "long",
+            "penalty": "8",
             "questions": [
                 {
-                    "question": "2+2=",
+                    "question": "2*14=",
+                    "good_answer": 28
+                },
+                {
+                    "question": "1*1=",
+                    "good_answer": 1
+                },
+                {
+                    "question": "11*11=",
+                    "good_answer": 121
+                },
+                {
+                    "question": "25*25=",
+                    "good_answer": 625
+                },
+                {
+                    "question": "1*4=",
                     "good_answer": 4
                 },
                 {
-                    "question": "2+0=",
+                    "question": "4/2=",
                     "good_answer": 2
                 }
             ]
+        },
+        {
+            "id": "negative",
+            "penalty": "4",
+            "questions": [
+                {
+                    "question": "2-2=",
+                    "good_answer": 0
+                },
+                {
+                    "question": "3-14=",
+                    "good_answer": -11
+                },
+                {
+                    "question": "10-100=",
+                    "good_answer": -90
+                },
+                {
+                    "question": "-1+0=",
+                    "good_answer": -1
+                }
+            ]    
         }
     ]
-    
 }`
